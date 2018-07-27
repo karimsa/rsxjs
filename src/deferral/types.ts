b0ca64f65ce90c5a78dd26184061289847d724f2
Karim Alibhai <karim@foko.co>
2018-07-27 06:14:55 -0400

    Add: debug logs to deferrals

--- src/deferral/types.ts
+++ src/deferral/types.ts
@@ -3,6 +3,9 @@
  * @copyright 2018-present Karim Alibhai. All rights reserved.
  */
 
+import createDebug from 'debug'
+const debug = createDebug('rsxjs')
+
 export type AsyncDeferral<T> = (defer: DeferFunction, ...args: any[]) => T
 
 export type CleanupFunction = () => Promise<void> | void
@@ -12,11 +15,13 @@ export class DeferredOperation {
   private deferred: CleanupFunction[] = []
 
   defer(cleanup: CleanupFunction): void {
+    debug(`registering deferred operation:`, cleanup)
     this.deferred.push(cleanup)
   }
 
   async cleanup(): Promise<void> {
     for (const cleanup of this.deferred) {
+      debug(`executing deferred operation:`, cleanup)
       await cleanup()
     }
   }
