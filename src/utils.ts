b022ff51affbb8d503d9298af001ace9685aa4f4
Karim Alibhai <karim@foko.co>
2018-07-28 15:10:44 -0400

    Add: doc comment to utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,9 @@
+/**
+ * @file src/utils.ts
+ * @description Some common utilities.
+ * @copyright 2018-present Karim Alibhai. All rights reserved.
+ */
+
 export function isDefined(v: any): boolean {
   return (
     v !== undefined &&
