a036e361269761287dfc47aef7eca60ef44d19ef
Karim Alibhai <karim@foko.co>
2018-08-01 13:16:09 -0400

    Fix: options to mutex fromAsync should be optional

--- src/mutex/from-async.ts
+++ src/mutex/from-async.ts
@@ -7,12 +7,14 @@ import { Mutex } from './mutex'
 import { AsyncFunction } from '../types'
 import { defaults, MutexOptionsGiven } from './types'
 
-export function fromAsync<T>(fn: AsyncFunction<T>, _options: MutexOptionsGiven): AsyncFunction<T> {
-  const options = defaults(_options)
+export function fromAsync<T>(
+  fn: AsyncFunction<T>,
+  options?: MutexOptionsGiven
+): AsyncFunction<T> {
   const m = new Mutex()
 
   return async function mutexWrappedFunction(this: any, ...args: any[]): Promise<T> {
-    const unlock = await m.lock(options.failFast)
+    const unlock = await m.lock(defaults(options).failFast)
 
     try {
       const result = await fn.apply(this, args)
