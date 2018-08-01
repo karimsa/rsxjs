681b60f5065c7e812ce8e0abd3960a93516dc75b
Karim Alibhai <karim@foko.co>
2018-08-01 15:10:36 -0400

    Add: module declaration file

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+/**
+ * @file index.d.ts
+ * @description Module declaration, exposing the entrypoint.
+ * @copyright 2018-present Karim Alibhai. All rights reserved.
+ */
+
+declare module 'rsxjs'
+export * from './dist'
+
