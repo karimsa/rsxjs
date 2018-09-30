0ed42b6dc687b18543c2c8a3da5ef32e9b885184
Karim Alibhai <karim@alibhai.co>
2018-09-30 14:43:19 -0400

    Update: deferral fromGenerator implementation to directly use co.wrap

--- src/deferral/from-generator.ts
+++ src/deferral/from-generator.ts
@@ -1,26 +1,14 @@
 /**
- * @file src/deferral/from-async.ts
+ * @file src/deferral/from-generator.ts
+ * @description Just another API for coroutines.
  * @copyright 2018-present Karim Alibhai. All rights reserved.
  */
 
 import { co } from '../coroutine'
 import { AsyncFunction } from '../types'
-import { DeferredOperation } from './types'
 
-import createDebug from 'debug'
-const debug = createDebug('rsxjs')
-
-export function fromGenerator<T>(fn: GeneratorFunction): AsyncFunction<T> {
-  async function deferralWrapper(this: any, ...args: any[]): Promise<T> {
-    const deferral = new DeferredOperation()
-
-    try {
-      return await co<T>(() => fn.call(this, deferral.defer.bind(deferral), ...args))
-    } finally {
-      debug(`unwinding deferred operations`)
-      await deferral.cleanup()
-    }
-  }
-
-  return deferralWrapper
+export function fromGenerator<T>(
+  fn: GeneratorFunction
+): AsyncFunction<T> {
+  return co.wrap<T>(fn)
 }
