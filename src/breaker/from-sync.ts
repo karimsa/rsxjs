740e434ccf521ccadb8c0d3f0f12431497ccdf7e
Karim Alibhai <karim@alibhai.co>
2018-10-06 17:39:54 -0400

    Revert: breaker.fromSync updates

--- src/breaker/from-sync.ts
+++ src/breaker/from-sync.ts
@@ -6,21 +6,46 @@
 import {
   defaults,
   BreakerOptionsGiven,
-  CircuitBreaker,
+  BreakerStateObject,
 } from './types'
 import {
   SyncFunction,
-  AsyncFunction,
 } from '../types'
 
 export function fromSync<T = any>(
   originalFn: SyncFunction<T>,
   _options?: BreakerOptionsGiven
-): AsyncFunction<T> {
+): SyncFunction<T> {
   const options = defaults(_options)
-  const breaker = new CircuitBreaker<T>(options)
+  const state: BreakerStateObject = {
+    numErrors: 0,
+    lastError: 'Unknown error',
+    lastErrorTime: 0,
+  }
 
-  return function syncBreaker(this: any, ...args: any[]): Promise<T> {
-    return breaker.attempt(() => originalFn.apply(this, args))
+  return function syncBreaker(this: any, ...args: any[]): T {
+    const callback = args.pop()
+    if (typeof callback !== 'function') {
+      throw new Error(`Last argument should be a callback function, not ${typeof callback}`)
+    }
+
+    if (
+      state.numErrors >= options.maxErrors &&
+      Date.now() - state.lastErrorTime < options.timeout
+    ) {
+      throw new Error(state.lastError)
+    }
+
+    try {
+      const result = originalFn.apply(this, args)
+      state.numErrors = 0
+      state.lastErrorTime = 0
+      return result
+    } catch (err) {
+      ++state.numErrors
+      state.lastError = err
+      state.lastErrorTime = Date.now()
+      throw err
+    }
   }
 }
