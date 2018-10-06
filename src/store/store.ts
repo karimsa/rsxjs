8087d3646960a90b6db21cbb8deb4b83bcbda8f1
Karim Alibhai <karim@alibhai.co>
2018-10-06 16:43:05 -0400

    Add: hash methods to stores

--- src/store/store.ts
+++ src/store/store.ts
@@ -18,6 +18,18 @@ export interface SetOptions {
 
 export interface Store {
   /**
+   * Increment a stored value atomically.
+   * @param key key to increment
+   */
+  incr(key: string): Promise<void>
+
+  /**
+   * Decrement a stored value atomically.
+   * @param key key to decrement
+   */
+  decr(key: string): Promise<void>
+
+  /**
    * Stores a given value at a given key.
    * @param key key to store value at
    * @param value the value to store
@@ -30,8 +42,54 @@ export interface Store {
    * @param key key to retreive from
    * @returns {Promise<T>} resolve to the value from the store, or rejects
    */
-  get<T>(key: string): Promise<T>
-  
+  get<T>(key: string): Promise<T | void>
+
+  /**
+   * Reads a value from the store.
+   * @param key key to retreive from
+   * @param defaultValue value to return if store doesn't have the key
+   * @returns {Promise<T>} resolve to the value from the store, or rejects
+   */
+  get<T>(key: string, defaultValue: T): Promise<T>
+
+  /**
+   * Stores a given value at a given key within a given hashmap.
+   * @param namespace name of the hashmap to store within
+   * @param key key to store value at
+   * @param value the value to store
+   * @returns {Promise<void>} resolves once key is stored
+   */
+  hset<T>(namespace: string, key: string, value: T, options?: SetOptions): Promise<void>
+
+  /**
+   * Reads a value from a hashmap in the store.
+   * @param namespace name of the hashmap to retrieve from
+   * @param key key to retreive from
+   * @returns {Promise<T>} resolve to the value from the store, or rejects
+   */
+  hget<T>(namespace: string, key: string): Promise<T | void>
+
+  /**
+   * Reads a value from a hashmap in the store.
+   * @param namespace name of the hashmap to retrieve from
+   * @param key key to retreive from
+   * @param defaultValue value to return if store doesn't have the key
+   * @returns {Promise<T>} resolve to the value from the store, or rejects
+   */
+  hget<T>(namespace: string, key: string, defaultValue: T): Promise<T>
+
+  /**
+   * Increment a stored value atomically within a hashmap.
+   * @param key key to increment
+   */
+  hincr(namespace: string, key: string): Promise<void>
+
+  /**
+   * Decrement a stored value atomically within a hashmap.
+   * @param key key to decrement
+   */
+  hdecr(namespace: string, key: string): Promise<void>
+
   /**
    * Removes a value from the store.
    * @param key key to delete
