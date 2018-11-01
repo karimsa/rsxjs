efc1092d307a7f8729a3de8fa9a1c58188343020
Karim Alibhai <karim@foko.co>
2018-11-01 14:02:13 -0400

    Update: further simplify channel operations and fix timeout issues

--- src/channel/channel.ts
+++ src/channel/channel.ts
@@ -4,13 +4,13 @@
  * @copyright 2018-present Karim Alibhai. All rights reserved.
  */
 
+import createDebug from 'debug'
 import { typeCheck } from '@foko/type-check'
 
-import { select } from './select'
-import * as Errors from '../errors'
-import { isDefined, any } from '../utils'
-import { after as timeoutAfter } from '../timeout/after'
+const debug = createDebug('rsxjs:channel')
+
 import { Deferred, defer } from '../types'
+import { isDefined, any, delay } from '../utils'
 
 interface IChannel<T> {
   // default R/W methods
@@ -37,120 +37,19 @@ interface TakeResult<T> {
   ok: boolean
 }
 
-class BufferedChannel<T> implements IChannel<T> {
+export class Channel<T> {
+  private isOpen: boolean = true
   private readonly buffer: T[] = []
-  private readonly putters: {
+  private putters: {
     signal: Deferred<void>
     value: T
   }[] = []
-  private readonly takers: Deferred<T>[] = []
-
-  constructor(
-    private readonly config: ChannelConfig
-  ) {}
-
-  private async _put(v: T, dir: 'left' | 'right'): Promise<void> {
-    // try to pass it to the next taker first
-    const nextTaker = dir === 'left' ? this.takers.pop() : this.takers.shift()
-    if (nextTaker) {
-      nextTaker.resolve(v)
-      return
-    }
-
-    // if buffer is full, add a signal listener that
-    // will wait for room before pushing
-    if (this.buffer.length === this.config.bufferSize) {
-      const signal = defer<void>()
-      this.putters.push({
-        signal,
-        value: v,
-      })
-
-      return signal.promise
-    }
-
-    if (dir === 'right') {
-      this.buffer.push(v)
-    } else {
-      this.buffer.unshift(v)
-    }
-  }
-
-  put(v: T): Promise<void> {
-    return this._put(v, 'right')
-  }
-
-  lput(v: T): Promise<void> {
-    return this._put(v, 'left')
-  }
-
-  async _take(dir: 'left' | 'right'): Promise<T> {
-    if (this.buffer.length === 0) {
-      const nextPutter = dir === 'left' ? this.putters.shift() : this.putters.pop()
-      if (nextPutter) {
-        nextPutter.signal.resolve()
-        return nextPutter.value
-      }
-
-      const p = defer<T>()
-      this.takers.push(p)
-      return p.promise
-    }
-
-    if (dir === 'left') {
-      return this.buffer.shift() as T
-    }
-    return this.buffer.pop() as T
-  }
-
-  take() {
-    return this._take('left')
-  }
-
-  rtake() {
-    return this._take('right')
-  }
-
-  _select(dir: 'left' | 'right'): TakeResult<T> {
-    if (this.buffer.length > 0) {
-      return {
-        value: dir === 'left' ? this.buffer.shift() : this.buffer.pop(),
-        ok: true,
-      }
-    }
-
-    // try to steal from a putter
-    const nextPutter = dir === 'left' ? this.putters.shift() : this.putters.pop()
-    if (nextPutter) {
-      nextPutter.signal.resolve()
-      return {
-        value: nextPutter.value,
-        ok: true,
-      }
-    }
-
-    return {
-      ok: false,
-    }
-  }
-
-  select() {
-    return this._select('left')
-  }
-
-  rselect() {
-    return this._select('right')
-  }
-}
-
-export class Channel<T> {
-  private readonly chan: IChannel<T> & symbol
-  private isOpen: boolean = true
+  private takers: Deferred<T>[] = []
 
   // this makes the select() magic work
   private selectSymbol = Symbol()
   private static chanMap: { [key in any]: Channel<any> } = {}
-  
+
   ;[Symbol.toPrimitive]() { return this.selectSymbol }
 
   static getChannel<T>(sym: symbol): chan<T> {
@@ -159,26 +58,18 @@ export class Channel<T> {
       throw new Error(`Failed to fetch closed channel`)
     }
 
-    return chan as chan<T>
+    return chan as any as chan<T>
   }
 
   constructor(
-    c: Partial<ChannelConfig> = {}
+    private readonly config: Partial<ChannelConfig> = {
+      bufferSize: 0,
+    }
   ) {
     Channel.chanMap[this.selectSymbol as any] = this
 
-    const config: ChannelConfig = {
-      bufferSize: 0,
-    }
-    
-    typeCheck('object', c)
-    typeCheck('number?', c.bufferSize)
-      
-    if (isDefined(c.bufferSize)) {
-      config.bufferSize = c.bufferSize as any
-    }
-
-    this.chan = new BufferedChannel(config) as any
+    typeCheck('object', config)
+    typeCheck('number?', config.bufferSize)
   }
 
   close(): void {
@@ -188,24 +79,64 @@ export class Channel<T> {
   
   private async _put(dir: 'left' | 'right', value: T, timeout?: number): Promise<PutResult> {
     if (!this.isOpen) {
-      throw new Error(Errors.CLOSED_CHAN)
+      debug(`refusing to put on a closed channel`)
+      return { ok: false }
     }
 
-    if (!isDefined(timeout)) {
-      if (dir === 'right') {
-        await this.chan.put(value)
-      } else {
-        await this.chan.lput(value)
-      }
-
+    // try to pass it to the next taker first
+    const nextTaker = dir === 'left' ? this.takers.pop() : this.takers.shift()
+    if (nextTaker) {
+      debug(`found pending taker, forwarding value`)
+      nextTaker.resolve(value)
       return { ok: true }
     }
 
-    const [i] = await any([
-      dir === 'right' ? this.chan.put(value) : this.chan.lput(value),
-      timeoutAfter(timeout).take(),
-    ])
-    return { ok: i === 0 }
+    // if buffer is full, add a signal listener that
+    // will wait for room before pushing
+    if (this.buffer.length === this.config.bufferSize) {
+      const signal = defer<void>()
+      const putter = {
+        signal,
+        value,
+      }
+      this.putters.push(putter)
+      debug(`added background putter, waiting for signal`)
+
+      if (isDefined(timeout)) {
+        const [i] = await any([
+          signal.promise,
+          delay(timeout),
+        ])
+
+        if (i === 0) {
+          debug(`background signal received for putter, value was read`)
+        } else {
+          debug(`background putter timed out`)
+
+          // unfortunately need to resort to O(n) since the indexes
+          // will shift while waiting
+          this.putters = this.putters.filter(putterInList => {
+            return putterInList !== putter
+          })
+        }
+
+        return {
+          ok: i === 0,
+        }
+      }
+
+      await signal.promise
+      return { ok: true }
+    }
+
+    if (dir === 'right') {
+      debug(`putter wrote value to the right of the buffer`)
+      this.buffer.push(value)
+    } else {
+      debug(`putter wrote value to the left of the buffer`)
+      this.buffer.unshift(value)
+    }
+    return { ok: true }
   }
 
   put(value: T, timeout?: number) {
@@ -217,24 +148,71 @@ export class Channel<T> {
   }
 
   private async _take(dir: 'left' | 'right', timeout?: number): Promise<TakeResult<T>> {
-    const { value, ok } = (dir === 'left' ? this.chan.select() : this.chan.rselect())
+    const { value, ok } = (dir === 'left' ? this.select() : this.rselect())
     if (ok) {
       return { value, ok }
     }
 
     if (!this.isOpen) {
-      throw new Error(Errors.CLOSED_CHAN)
+      debug(`channel is closed & empty, refusing to take`)
+      return { ok: false }
     }
 
-    if (!isDefined(timeout)) {
-      const value = await (dir === 'left' ? this.chan.take() : this.chan.rtake())
-      return { value, ok: true }
+    if (this.buffer.length === 0) {
+      const nextPutter = dir === 'left' ? this.putters.shift() : this.putters.pop()
+      if (nextPutter) {
+        nextPutter.signal.resolve()
+        debug(`taker found an empty buffer & a background putter, stealing value`)
+        return {
+          ok: true,
+          value: nextPutter.value,
+        }
+      }
+
+      const p = defer<T>()
+      this.takers.push(p)
+      debug(`adding taker to background list`)
+
+      if (isDefined(timeout)) {
+        const [i, value] = await any([
+          p.promise,
+          delay(timeout),
+        ])
+
+        if (i === 0) {
+          debug(`received value from background taker`)
+          return {
+            ok: true,
+            value,
+          }
+        }
+
+        debug(`failed to received value from background taker, timed out`)
+        this.takers = this.takers.filter(takerFromList => {
+          return takerFromList !== p
+        })
+        return {
+          ok: false,
+        }
+      }
+
+      const value = await p.promise
+      debug(`received value from background taker`)
+      return {
+        ok: true,
+        value,
+      }
     }
 
-    return select({
-      [this.chan]: (value: T) => ({ value, ok: true }),
-      [timeoutAfter(timeout)]: () => ({ ok: false }),
-    })
+    const poppedValue = dir === 'left' ? 
+      this.buffer.shift() as T :
+      this.buffer.pop() as T
+
+    debug(`read value from static buffer`)
+    return {
+      ok: !!poppedValue,
+      value: poppedValue,
+    }
   }
 
   take(timeout?: number) {
@@ -245,12 +223,38 @@ export class Channel<T> {
     return this._take('right', timeout)
   }
 
+  private _select(dir: 'left' | 'right') {
+    if (this.buffer.length > 0) {
+      debug(`selected value from %s of buffer`, dir)
+      return {
+        value: dir === 'left' ? this.buffer.shift() : this.buffer.pop(),
+        ok: true,
+      }
+    }
+
+    // try to steal from a putter
+    const nextPutter = dir === 'left' ? this.putters.shift() : this.putters.pop()
+    if (nextPutter) {
+      debug(`selected value from background putter`)
+      nextPutter.signal.resolve()
+      return {
+        value: nextPutter.value,
+        ok: true,
+      }
+    }
+
+    debug(`failed to select a value`)
+    return {
+      ok: false,
+    }
+  }
+
   select() {
-    return this.chan.select()
+    return this._select('left')
   }
 
   rselect() {
-    return this.chan.rselect()
+    return this._select('right')
   }
 
   async* [Symbol.asyncIterator](): AsyncIterableIterator<T> {
@@ -302,7 +306,7 @@ export class WriteOnlyChannel<T> {
 
 export type readOnlyChan<T> = ReadOnlyChannel<T> & symbol
 export type writeOnlyChan<T> = WriteOnlyChannel<T> & symbol
-export type chan<T> = Channel<T> & symbol
+export type chan<T> = IChannel<T> & symbol
 
 export function makeChan<T>(opts?: Partial<ChannelConfig>): chan<T> {
   return new Channel(opts) as any
